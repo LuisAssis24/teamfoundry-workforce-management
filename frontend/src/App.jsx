e538d166c1afba6e92449f7abc1cea76adaa53b4
Gabriell0liv <gabrielr.oliveira@ua.pt>
2025-11-05 19:21:21 +0000

    Refactor SuperAdmin page imports and rename files for consistency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import CandidateLogin from "./pages/login-pages/CandidateLogin/Login.jsx";
 import AdminLogin from "./pages/login-pages/AdminLogin/AdminLogin.jsx";
-import Credenciais from "./pages/login-pages/superAdmin/credenciais.jsx";
-import GestaoTrabalho from "./pages/login-pages/superAdmin/gestaoTrabalho.jsx";
-import GestaoSite from "./pages/login-pages/superAdmin/gestaoSite.jsx";
-import Metricas from "./pages/login-pages/superAdmin/metricas.jsx";
+import Credenciais from "./pages/SuperAdmin/Credenciais.jsx";
+import GestaoTrabalho from "./pages/SuperAdmin/GestaoTrabalho.jsx";
+import GestaoSite from "./pages/SuperAdmin/GestaoSite.jsx";
+import Metricas from "./pages/SuperAdmin/Metricas.jsx";
 import SuperAdminLayout from "./components/layout/SuperAdminLayout.jsx";
 
 function App() {
